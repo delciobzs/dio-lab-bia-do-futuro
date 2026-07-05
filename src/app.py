2ad0252371e0cadd58b8280dc1b089594f236244
delciobzs <delcio.bzs@gmail.com>
2026-07-05 04:10:17 -0300

    feat: estruturar interface de chat e gerenciamento de estado do session_state, adiciona gitignore e pyproject

---
+++ src/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+def app():
+    st.header("MorFi", help="MorFi: eu aplicativo financeiro")
+    st.write("Seu gerenciador financeiro inteligente. ### :green[Sempre à sua maneira]")
+
+    if "mensagens" not in st.session_state:
+        st.session_state["mensagens"] = []
+
+    for mensagem in st.session_state["mensagens"]:
+        with st.chat_message(mensagem["usuario"]):
+            st.write(mensagem["texto"])
+
+    mensagem_usuario = st.chat_input("No que posso te ajudar hoje, Delcio?")
+
+    if mensagem_usuario:
+        st.session_state["mensagens"].append({
+            "usuario": "user",
+            "texto": mensagem_usuario
+        })
+
+        st.session_state["mensagens"].append({
+            "usuario": "assistant",
+            "texto": "O agente de IA do MorFi será adicionado em breve! 🚀"
+        })
+
+        st.rerun()
